ef261b9d66f1531ed9052c4a715e15e04227827c
jorgesanchezs <jorge.peekitas@gmail.com>
2024-08-02 12:57:50 +0200

    chore: Add Instagram login credentials to config file

--- config/config.py
+++ config/config.py
@@ -9,5 +9,7 @@ load_dotenv()
 # Configuration file for the application
 INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
-CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL', 600))
+CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL'))
 LAST_POST_FILE = os.getenv('LAST_POST_FILE', 'data/last_post.txt')
+INSTAGRAM_LOGIN_USERNAME = os.getenv('INSTAGRAM_LOGIN_USERNAME')
+INSTAGRAM_LOGIN_PASSWORD = os.getenv('INSTAGRAM_LOGIN_PASSWORD')
